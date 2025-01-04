8f3de070cc3a8eb21404e38bd2ceb09bb7f3a44f
Keerthivasan-11 <keerthivasangopal2004@gmail.com>
2025-01-04 21:51:56 +0530

    app.py

--- app.py
+++ app.py
@@ -1,32 +1,39 @@
 import gspread
-from oauth2client.client import OAuth2WebServerFlow
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
 import streamlit as st
-from oauth2client.file import Storage
-from oauth2client.tools import run_flow
+import os
 
-# Replace the following credentials with your client ID and client secret from Google Cloud Console
-CLIENT_ID = '39633506830-9n7121fp8qh6a9tvv0h4mnhr871bv4ht.apps.googleusercontent.com'
-CLIENT_SECRET = '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'
-REDIRECT_URI = 'http://localhost:8501'  # Use your Streamlit app's URL
+# The scopes required by the API to access Google Sheets
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive']
 
-# Scope to access Google Sheets
-SCOPE = ['https://www.googleapis.com/auth/spreadsheets.readonly', 'https://www.googleapis.com/auth/drive']
+# Path to your credentials.json file
+CREDS_FILE = 'credentials.json'
 
-# Create an OAuth2 flow object
-flow = OAuth2WebServerFlow(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, scope=SCOPE, redirect_uri=REDIRECT_URI)
-
-# Function to authenticate using OAuth2
+# Function to authenticate and authorize using OAuth2
 def authenticate_oauth2():
-    storage = Storage('credentials.json')
-    credentials = storage.get()
-
-    if not credentials or credentials.invalid:
-        credentials = run_flow(flow, storage)
+    creds = None
+    # If we have saved credentials, load them
+    if os.path.exists(CREDS_FILE):
+        creds = Credentials.from_authorized_user_file(CREDS_FILE, SCOPES)
     
-    client = gspread.authorize(credentials)
+    # If no credentials or they are invalid, prompt the user to log in
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDS_FILE, SCOPES)
+            creds = flow.run_local_server(port=0)
+        
+        # Save the credentials for the next run
+        with open(CREDS_FILE, 'w') as token:
+            token.write(creds.to_json())
+    
+    # Use the credentials to authorize the client
+    client = gspread.authorize(creds)
     return client
 
-# Example function to access Google Sheets data
+# Function to get data from a Google Sheet
 def get_spreadsheet_data(spreadsheet_id, range_name):
     client = authenticate_oauth2()
     sheet = client.open_by_key(spreadsheet_id)
