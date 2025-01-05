68267233a3dc84ded49a0bf09e089165ff025d0f
Keerthivasan-11 <keerthivasangopal2004@gmail.com>
2025-01-05 19:16:50 +0530

    app.py

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
-import streamlit as st
+import toml
 import gspread
-from google.auth.transport.requests import Request
 from google.oauth2.service_account import Credentials
+import streamlit as st
 
-# Authenticate using credentials stored in secrets.toml
-scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
-credentials = Credentials.from_service_account_info(st.secrets["gsheets"], scopes=scope)
+# Load credentials from secrets.toml
+def load_secrets():
+    secrets = toml.load(".streamlit/secrets.toml")["connections"]["gsheets"]
+    return secrets
+
+# Load the secrets from the TOML file
+secrets = load_secrets()
+
+# Prepare credentials dictionary
+credentials_dict = {
+    "type": secrets["type"],
+    "project_id": secrets["project_id"],
+    "private_key_id": secrets["private_key_id"],
+    "private_key": secrets["private_key"],
+    "client_email": secrets["client_email"],
+    "client_id": secrets["client_id"],
+    "auth_uri": secrets["auth_uri"],
+    "token_uri": secrets["token_uri"],
+    "auth_provider_x509_cert_url": secrets["auth_provider_x509_cert_url"],
+    "client_x509_cert_url": secrets["client_x509_cert_url"]
+}
+
+# Authenticate using service account credentials
+credentials = Credentials.from_service_account_info(credentials_dict, scopes=["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"])
 
 # Use gspread to access Google Sheets
 client = gspread.authorize(credentials)
 
-# Access the spreadsheet using the URL from secrets.toml
-spreadsheet_url = st.secrets["gsheets"]["spreadsheet_url"]
+# Access the spreadsheet
+spreadsheet_url = secrets["spreadsheet"]
 spreadsheet = client.open_by_url(spreadsheet_url)
-worksheet = spreadsheet.worksheet("Sheet1")  # Replace "Registrations" with your worksheet name
+worksheet = spreadsheet.worksheet("Registrations")
 
 # Registration Form
 st.title("ISA Hackathon Registration")
