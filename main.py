9768a3669784a5a57c18cf5e890a35e6a40b01d4
Keerthivasan-11 <keerthivasangopal2004@gmail.com>
2025-01-23 22:39:25 +0530

    Update main.py

--- main.py
+++ main.py
@@ -148,7 +148,7 @@ def display_scrolling_content():
 with st.sidebar:
     selected_option = option_menu(
         menu_title="ISA MIT Student Chapter",
-        options=["Home","Laboratory Facilities", "2024 Events","International Society of Automation Officer", "Registration form", "Gform registration", "About", "Contact"],
+        options=["Home", "Laboratory Facilities", "2024 Events", "International Society of Automation Officer", "Registration form", "Gform registration", "About", "Contact"],
         icons=["house-fill", "calendar-event-fill", "person-plus-fill", "book-fill", "info-circle-fill", "envelope-fill"],
         menu_icon="gear-fill",
         default_index=0,
@@ -173,9 +173,8 @@ elif selected_option == "Laboratory Facilities":
     app4.app()
 elif selected_option == "2024 Events":
     app5.app()
-elif selected_option=="International Society of Automation Officer":
+elif selected_option == "International Society of Automation Officer":
     app8.app()
-    
 elif selected_option == "About":
     app6.app()
 elif selected_option == "Contact":
