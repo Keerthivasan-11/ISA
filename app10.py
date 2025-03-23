83135867d8ec80d61cbb45ba680d927bdbd7bf94
Keerthivasan-11 <keerthivasangopal2004@gmail.com>
2025-03-23 12:54:45 +0530

    Update app10.py

--- app10.py
+++ app10.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def app():
-    st.title("Indian Engineers Elite Challenge 2022 - Thanksgiving Video")
+    st.title("Indian Engineers Elite Challenge 2025 - Thanksgiving Video")
     
     st.write("## Watch the Official Thanksgiving Video")
     st.video("https://raw.githubusercontent.com/Keerthivasan-11/ISA/main/WhatsApp%20Video%202025-03-22%20at%2018.58.57.mp4")
