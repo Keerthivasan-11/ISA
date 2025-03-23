a4d041e9fe1c267fd6ce813178c9c224b1795d38
Keerthivasan-11 <keerthivasangopal2004@gmail.com>
2025-03-23 12:46:47 +0530

    Update app10.py

--- app10.py
+++ app10.py
@@ -4,7 +4,7 @@ def app():
     st.title("Indian Engineers Elite Challenge 2022 - Thanksgiving Video")
     
     st.write("## Watch the Official Thanksgiving Video")
-    st.video("")
+    st.video("https://github.com/Keerthivasan-11/ISA/blob/main/WhatsApp%20Video%202025-03-23%20at%2010.34.44.mp4")
     
 
 
